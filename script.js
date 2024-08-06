3e4e7e584bf1bfbd401bc38c0ab0a992be7673bd
aserafim <correia.alefe@gmail.com>
2024-08-06 11:58:46 -0300

    Corrigida utilização dos operados e botão AC

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-document.querySelectorAll('.calculator__key:not(.calculator__key--operator):not(.calculator__key--enter)').forEach(button => {
+document.querySelectorAll('.calculator__key:not(.calculator__key--enter):not(.calculator__key--clean)').forEach(button => {
   button.addEventListener('click', function(){
     const outPutDOM = document.querySelector('.calculator__output');
     const buttonValue = button.innerText;
     if(outPutDOM.innerText == 0){
       outPutDOM.innerText = buttonValue;
-    }
-    else{
+    } else {
       outPutDOM.innerText += buttonValue;
     }
-
   });
-  
 });
 
 function limpar(){
-  const outPutDOM = document.querySelector('.calculator__output')
-  outPutDOM.innerText = 0
+  const outPutDOM = document.querySelector('.calculator__output');
+  outPutDOM.innerText = 0;
+}
+
+function resultado(){
+  const operacao = document.querySelector('.calculator__output');
+  
+
 }

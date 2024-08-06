fc3e10915a722e0b834b3f8ac97966d6b5f480b7
aserafim <correia.alefe@gmail.com>
2024-08-06 11:26:39 -0300

    Corrigido zero a esquerda

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-document.querySelectorAll('.calculator__key').forEach(button => {
+document.querySelectorAll('.calculator__key:not(.calculator__key--operator):not(.calculator__key--enter)').forEach(button => {
   button.addEventListener('click', function(){
     const outPutDOM = document.querySelector('.calculator__output');
     const buttonValue = button.innerText;
-    outPutDOM.innerText += buttonValue;
+    if(outPutDOM.innerText == 0){
+      outPutDOM.innerText = buttonValue;
+    }
+    else{
+      outPutDOM.innerText += buttonValue;
+    }
 
   });
   
 });
+
+function limpar(){
+  const outPutDOM = document.querySelector('.calculator__output')
+  outPutDOM.innerText = 0
+}
